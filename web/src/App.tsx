dccb627ccfbc2139e38cf47c7047e984e4d96ad1
Andre Rodrigues Lima <andrezaga9@hotmail.com>
2022-09-14 08:00:15 +1000

    mobile and web styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,45 @@
-import './styles/main.css'
+import "./styles/main.css";
+import logoImg from "./assets/logo-esport.svg";
 function App() {
   return (
-    <div className=""></div>
-  )
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center ny-20">
+      <img src={logoImg} alt="" />
+      <h1 className="text-6xl text-white font-black mt-20">
+        Your{" "}
+        <span className="bg-gradient text-transparent bg-clip-text">Dual</span>{" "}
+        is here
+      </h1>
+      <div className="grid grid-cols-6 gap-6 mt-16">
+        <a href="" className="relative rounded-lg overflow-hidden">
+          <img src="/game-1.png" alt="" />
+          <div className="w-full pt-16 pb-4 px-4 bg-game-gradiant absolute bottom-0 left-0 right-0">
+            <strong className="font-bold text-white"></strong>
+            <span className="text-zinc-300 text-sn block"></span>
+          </div>
+        </a>
+        <a href="">
+          <img src="/game-2.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-3.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-4.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-5.png" alt="" />
+        </a>
+        <a href="">
+          <img src="/game-6.png" alt="" />
+        </a>
+      </div>
+      <div className="pt-1 bg-gradient mt-8 self-stretch rounded-lg overflow-hidden">
+        <div className="bg-[#2A2634] px-8 py-6  ">
+
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -6,8 +6,13 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+
+      },
       backgroundImage:{
         background: "url('/background.jpg')",
+        'gradient' : 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 44.57%)',
+        'game-gradiant':'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)'
       }
     },
   },
